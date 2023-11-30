e9d3b8f23f96223e158aa126a64e26ea7e115c20
StudentELvdH <lohmeijer.vandenhul@gmail.com>
2023-11-30 14:13:41 +0100

    added books

---
+++ books/app.py
@@ -0,0 +1,33 @@
+import os
+
+from flask import Flask, session
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+from models import *
+
+app = Flask(__name__)
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+db.init_app(app)
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+Session(app)
+
+
+@app.route("/")
+def index():
+    return "Project 1: TODO - deze stap uitwerken"
+
+@app.route("/book/<isbn>", methods=["GET"])
+def book(isbn):
+    return "TODO: extend with correct ISBN number"
+
---
+++ books/templates/application.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/book/<isbn>")
+#def index():
+
+#def book(isbn):
+    #return render_template("bookpage.html")
+
+def book(isbn):
+    # """Book a flight."""
+    # get the book with the right isbn from the database. 
+    # Then pass on that book to the template and have the template render the title, isbn, etc.
+    
+    # Get form information.
+    isbn = request.form.get("book")
+    try:
+        isbn = str(request.form.get("isbn"))
+    except ValueError:
+        return render_template("error.html", message="Invalid isbn")
+
+    # Make sure isbn exists.
+    if db.execute("SELECT * FROM Book WHERE isbn = :isbn", {"isbn": isbn}).rowcount == 0:
+        return render_template("error.html", message="No such book with that isbn")
+    db.execute("INSERT INTO Book (name, flight_id) VALUES (:name, :flight_id)",
+            {"name": name, "flight_id": flight_id})
+    db.commit()
+    return render_template("bookpage.html")
